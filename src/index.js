af942353a4b0c9ef20daa096d68bad09cc833af1
ThanosApostolou <thanosapostolou@outlook.com>
2020-03-05 19:12:15 +0200

    initial rest api

--- src/index.js
+++ src/index.js
@@ -2,5 +2,9 @@ var number_of_nodes = process.argv[2];
 var capacity = process.argv[3];
 var difficulty = process.argv[4];
 
+NODE=require("./node.js")
 
 console.log("Running with number_of_nodes: " + number_of_nodes + " capacity: " + capacity + " difficulty: " + difficulty);
+for (id=0; id<number_of_nodes; id++) {
+    mynode = new NODE(id);
+}
