9e5b538549c80424eb2efeb0dda07049eefc93a5
ThanosApostolou <thanosapostolou@outlook.com>
2020-03-05 18:20:34 +0200

    our initial code

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var number_of_nodes = process.argv[2];
+var capacity = process.argv[3];
+var difficulty = process.argv[4];
+
+
+console.log("Running with number_of_nodes: " + number_of_nodes + " capacity: " + capacity + " difficulty: " + difficulty);
