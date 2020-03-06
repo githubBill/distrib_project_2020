2d659ac496e014115260e69e6614f68ce0870845
ThanosApostolou <thanosapostolou@outlook.com>
2020-03-06 17:06:45 +0200

    restructure

--- src/node.js
+++ src/node.js
@@ -1,11 +1,40 @@
-REST=require("./rest.js")
-WALLET=require("./wallet.js")
+const jwa = require('jwa');
+
+REST = require("./rest.js")
+WALLET = require("./wallet.js")
+
 
 module.exports = class NODE {
+    nodeid;
+    wallet;
+    restapp;
+    blockchain;
+
     constructor(id) {
         this.nodeid = id;
         this.wallet = new WALLET();
         this.restapp = new REST(this.nodeid);
         this.blockchain = [];
     }
+
+    sign_transaction (transaction){
+        const rsa_sign = jwa('RS256');
+        const input = transaction ; //string
+        const signature = rsa_sign.sign(input,this.wallet.privatekey);
+        return signature;
+    };
+
+    verify_signature (signature, transaction, publicKey){
+        const rsa_sign = jwa('RS256');
+        const input = transaction ; //string
+        var check = rsa_sign.verify(input, signature, publicKey);
+        return check ;
+    };
+}
+
+function test() {
+    this.text="test";
+    this.sig=this.sign_transaction(this.text);
+    this.check=this.verify_signature(this.sig, this.text, this.wallet.publickey);
+    console.log(this.sig, "\n", this.check);
 }
--- src/wallet.js
+++ src/wallet.js
@@ -1,4 +1,13 @@
+const NodeRSA = require('node-rsa');
+
 module.exports = class WALLET {
+    privatekey;
+    publickey;
+
     constructor() {
+        const key = new NodeRSA({b: 256});
+        key.generateKeyPair();
+        this.privatekey = key.exportKey('private');
+        this.publickey = key.exportKey('public');
     }
 }
